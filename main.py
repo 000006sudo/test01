b67582bdc67a5a290d5552e72ee5728d19d845d0
000006sudo <55531605+000006sudo@users.noreply.github.com>
2024-04-14 22:10:38 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+def on_reload():
+    st.sidebar.title('AIMS Test UI-01')
+    st.sidebar.header('ログイン')
+    st.sidebar.text_input('アカウント')
+    st.sidebar.text_input('パスワード')
+    st.sidebar.button('ログイン')
+    st.sidebar.header('設定')
+    st.sidebar.text('なにかしらの設定項目')
+ 
+    st.title('A Innovative Matching System')
+    st.header('Chat room')
+    st.text('何でも聞いてね！')
+
+    st.text_area('質問')
+    req = st.button('送信')
+
+    if req:
+        st.text('メッセージ送信')
+
+
+    return
+
+
+def main():
+    on_reload()
+    return
+
+if __name__ == '__main__':
+    main()
